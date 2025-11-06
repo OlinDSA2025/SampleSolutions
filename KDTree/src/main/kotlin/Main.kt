af1c16bc576872fb455d005d1ade5c87a9acc6c0
Paul Ruvolo <paullundyruvolo@gmail.com>
2025-11-06 10:46:25 -0500

    changed reference to k instead of d

--- KDTree/src/main/kotlin/Main.kt
+++ KDTree/src/main/kotlin/Main.kt
@@ -8,36 +8,36 @@ import kotlin.time.Duration
  * 1000 test points will be generated to test against the training
  * points.
  *
- * @param d: the dimensionality of the dataset to create
+ * @param k: the dimensionality of the dataset to create
  * @param numPoints: the number of points to use to match against
  *   (1000 test points will be used)
  * @return
  */
-fun runExperiment(d: Int, numPoints: Int): Triple<Duration, Duration, Duration> {
+fun runExperiment(k: Int, numPoints: Int): Triple<Duration, Duration, Duration> {
     // mesh grid
     val pointsList = mutableListOf<DoubleArray>()
     val labels = mutableListOf<String>()
     for (i in 0 until numPoints) {
-        val p = DoubleArray(d) { Random.nextDouble() }
+        val p = DoubleArray(k) { Random.nextDouble() }
         pointsList.add(p)
         labels.add("$i")
     }
     val points = pointsList.toTypedArray()
     val labelsArray = labels.toTypedArray()
-    val x = KDTree<String>(d = d)
+    val x = KDTree<String>(k = k)
     val t1 = measureTime {
         x.buildTree(points, labelsArray)
     }
     println(x.toString())
     val t2 = measureTime {
         for (i in 0 until 1000) {
-            val p = DoubleArray(d) { Random.nextDouble() }
+            val p = DoubleArray(k) { Random.nextDouble() }
             x.query(p)
         }
     }
     val t3 = measureTime {
         for (i in 0 until 1000) {
-            val p = DoubleArray(d) { Random.nextDouble() }
+            val p = DoubleArray(k) { Random.nextDouble() }
             val g = points.minBy { p.squaredDistanceTo(it) }
         }
     }
